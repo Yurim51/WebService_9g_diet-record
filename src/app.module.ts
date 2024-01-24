ad3db64074bc3e6c178267bc75c9abb3ccb1ee7b
[AI9] 김명지 <kmjoyit21045@gmail.com>
2024-01-24 14:34:31 +0900

    Revert "Fix: dotenv"
    This reverts commit 7fd4aecef8124cea0c97ef1dd29f99d843ca7dac.
    
    브랜치를 나눈 시점으로 돌아가고자하였습니다.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,12 @@ import { AuthModule } from './auth/auth.module';
 import { PassportModule } from '@nestjs/passport';
 import { UserModule } from './user/user.module';
 import { ImageModule } from './image/image.module';
+import * as dotenv from "dotenv";
+import * as config from "config";
 
+dotenv.config({path: __dirname + '/../.env'});
+const dbConfig = config.get("db");
+console.log(typeORMConfig)
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
